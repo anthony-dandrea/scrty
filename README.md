b136ad8d04540d941352f2d3bb67690e7fddcc87
Anthony D'Andrea <anthony.dandrea@me.com>
2015-08-09 15:22:12 -0700

    simple login with launchkey. logout button added as well.

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 Experiments with LaunchKey as a security wall for password consolidation service
 
 [Workflow](https://docs.google.com/presentation/d/1_47svWSrEbkvqWVnYRCl0bbJ_Yb2HVGqY4mgOj75aIM/edit#slide=id.p)
+
+Setup:
+ - Ask Anthony for keys in the `config.py` and the `lk_private_key.key` files.
+ - Download LaunchKey App and pair your device.
+ - Ask Anthony for access to PWW org on [lkey dashboard](https://dashboard.launchkey.com).
--- scrty.py
+++ scrty.py
@@ -1,20 +1,66 @@
-from flask import Flask, render_template, jsonify, request, make_response
-from random import randint
-import jinja2
+from flask import Flask, render_template, jsonify, request, make_response, session, redirect, abort
+from flask.ext.launchkey import LaunchKeyManager, login_required, current_user
 from flask.ext.assets import Environment, Bundle
-from htmlmin.minify import html_minify
 from flask.ext.bcrypt import Bcrypt
+import jinja2, config, random, string
+from htmlmin.minify import html_minify
 
+###################
+## Configuration
+###################
 app = Flask(__name__)
 assets = Environment(app)
 bcrypt = Bcrypt(app)
+app.config.from_pyfile('config.py')
+# LaunchKey Config
+launchkey = LaunchKeyManager(app)
+launchkey.login_view = "/login"
 
-# Routes
-@app.route("/")
+###################
+## Routes
+###################
+@app.route('/')
 def index():
-    return html_minify(unicode(render_template('index.html')).encode('utf-8'))
+    # Send users to login if they are not authenticated
+    if current_user.is_anonymous():
+        return redirect('/login')
+    else:
+        return html_minify(unicode(render_template('index.html')).encode('utf-8'))
 
-# Endpoints
+@app.route("/login")
+def login():
+    return html_minify(unicode(render_template('login.html')).encode('utf-8'))
+
+###################
+## Endpoints
+###################
+@app.route('/login', methods=['POST'])
+def login_endpoint():
+    username = str(jinja2.utils.escape(request.form['username']))
+    auth_request = launchkey.authorize(username)
+    return jsonify({'response': auth_request})
+
+@app.route('/poll', methods=['GET'])
+def poll():
+    # Check for a user response to login request
+    poll_response = launchkey.poll_request()
+    return jsonify({'response': poll_response })
+
+@app.route('/isauthorized', methods=['GET'])
+def is_authorized():
+    # Check if the user is authorized
+    auth_response = launchkey.is_authorized()
+    if auth_response:
+        # User has been authorized, so log them in
+        launchkey.login()
+    return jsonify({'response': auth_response })
+
+@app.route('/logout', methods=['GET'])
+def deauth_user():
+    # Logout user from both LaunchKey as well as Flask-Login
+    success = launchkey.logout()
+    return jsonify({'response': success})
+
 @app.route("/encrypt-password", methods=['POST'])
 def encrypt_password():
     """
@@ -30,6 +76,25 @@ def encrypt_password():
     response["success"] = True
     response["encrypted_password"] = encrypt_pass
     return jsonify(response)
+
+###################
+## CSRF Protection
+## See: http://flask.pocoo.org/snippets/3/
+###################
+
+@app.before_request
+def csrf_protect():
+    if request.method == "POST":
+        token = session.pop('_csrf_token', None)
+        if not token or token != request.form.get('_csrf_token'):
+            abort(403)
+
+def generate_csrf_token():
+    if '_csrf_token' not in session:
+        session['_csrf_token'] = ''.join(random.choice(string.ascii_uppercase + string.digits) for c in range(30))
+    return session['_csrf_token']
+
+app.jinja_env.globals['csrf_token'] = generate_csrf_token
 
 if __name__ == '__main__':
     Environment.auto_build = True
